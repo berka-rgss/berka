631de9d8af9af33497566c0ec3381444e91f5ded
berka-rgss <rpgmxp@hotmail.fr>
2014-02-12 14:16:46 +0100

    1 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Berka::Application.config.secret_key_base = '4da66f2cc20ce48505496a0354855a1138f8b68d62fd96b43f1487399f74f1e9bbff6078ce3f64f3e206cb94eb93fd064ac80815e0a50bff767ea76845b27f9e'
